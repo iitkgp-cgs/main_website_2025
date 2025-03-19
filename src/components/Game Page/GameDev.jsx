ef90b4c0d3e939df7f91f0695ce2eccda5562792
sakethakella <63924046+sakethakella@users.noreply.github.com>
2025-03-19 17:29:53 +0530

    Project zrh added

--- src/components/Game Page/GameDev.jsx
+++ src/components/Game Page/GameDev.jsx
@@ -23,13 +23,23 @@ const GameDev = () => {
     { id: 1, name: 'SOUL MAGICIAN', image: 'https://images.ctfassets.net/7ghyf81o1fuo/6qW0Sjsv4lok77z8oOpKIo/a7a21f32fc337b3168cbaf1f03e7a1b2/parag.png', creator: 'Parag Chimankar', linkedin: 'https://www.linkedin.com/in/chimankarparag/' ,play:'https://chimankarparag.itch.io/soul-magician' , description:'2D rpg map clear game', video:parag },
     { id: 2, name: 'socerers-realm', image: 'https://images.ctfassets.net/7ghyf81o1fuo/5is9mWnsC0Np5w8sFgv7M9/a3a2a63b37e28c8cd9ae4dcd6f0c6a83/Vansh.png', creator: 'Vansh Pali', linkedin: 'https://www.linkedin.com/in/vansh-pali-47b315287/' ,play:'https://vanshpali24.itch.io/socerers-realm' , description:'2D dungeon based game', video:'https://videos.ctfassets.net/7ghyf81o1fuo/6d6EhXGpplKEWkRdT7WcL5/a5e74c4118a914448542a97dd550d455/REC-20241006183232.mp4'},
     { id: 3, name: 'Backless', image: 'https://images.ctfassets.net/7ghyf81o1fuo/6RI1XPIY9thmmhbkycdzlJ/42318543a44f31a30e0463f6c03171f7/harswardhan.png', creator: 'Harshwardhan Walunjkar', linkedin: 'https://www.linkedin.com/in/harshwardhan-walunjkar-93035628b/' ,play:'https://harshuiuiui.itch.io/backless' , description:'An endless runner with a twist, lose all your lives, and the direction should reverse, only now your score starts decreasing. Collect all orbs and return to your path!', video:'https://videos.ctfassets.net/7ghyf81o1fuo/EPsNRkL2IuzehrWUuFLbI/1e3a3102021fb771c3a5bedfa7c8b4ed/harshwardhan.mp4'},
-    { id: 4, name: 'Chrono-chrome', image: 'https://res.cloudinary.com/dodlxtpvx/image/upload/v1738503328/Screenshot_2025-02-02_at_7.02.50_PM_yhycws.png', creator: 'Harshwardhan Walunjkar', linkedin: 'https://www.linkedin.com/in/harshwardhan-walunjkar-93035628b/' ,play:'https://harshuiuiui.itch.io/chrono-chrome' , description:'Collect colors from the tiles to pass through the laser of that color. Collect all purple orbs and reach the purple tile to finish the level. You can rewind time or spawn a clone of yourself that will trace and repeat your previous motions! The objective is simple enough solve the 5 levels!', video:'https://res.cloudinary.com/dodlxtpvx/video/upload/v1738503681/REC-20250202190931_epqqtr.mp4'},
-
+    { id: 4, name: 'Chrono-chrome', image: 'https://res.cloudinary.com/dodlxtpvx/image/upload/v1738503328/Screenshot_2025-02-02_at_7.02.50_PM_yhycws.png', creator: 'Harshwardhan Walunjkar', linkedin: 'https://www.linkedin.com/in/harshwardhan-walunjkar-93035628b/' ,play:'https://harshuiuiui.itch.io/chrono-chrome' , description:'Collect colors to pass lasers, grab all purple orbs, and reach the goal. Rewind time or use a clone to solve 5 levels!', video:'https://res.cloudinary.com/dodlxtpvx/video/upload/v1738503681/REC-20250202190931_epqqtr.mp4'}
+    
   ];
   const Game3D = [  
         { id: 6, name: 'Sins Unveiled ', image: 'https://images.ctfassets.net/7ghyf81o1fuo/4pQ4TYJSKCV4xRYsRgSbs0/2d85e89f9e7b8592d3e783f3d3f2befc/shaurya.png', creator: 'Shaurya', linkedin: 'https://www.linkedin.com/in/shaurya-baranwal-6a240817b/',play:'',description:'3d Dark Horror Game ',video:'https://videos.ctfassets.net/7ghyf81o1fuo/3gaqWRh0PGHgNO3SKEIP9s/bc8c3e99e6b3afdf8f866e35cbb61b8c/shaurya.mp4' },
         { id: 7, name: '3d Parkour Game', image: 'https://images.ctfassets.net/7ghyf81o1fuo/016o8C8VM9Id3z4dNN5bSF/8581c6b1fbb94648b922f410845820d5/shazain.png', creator: 'Md Shahzain Misbah', linkedin: 'https://www.linkedin.com/in/md-shahzain-misbah-593724294/',play:'https://kaxyon.itch.io/parkour-afterlife',description:'3D World where you have to complete challenging levels. ',video:'' },
         { id: 8, name: 'Point Blank', image: 'https://images.ctfassets.net/7ghyf81o1fuo/6gHMd7wcqO463x4AjYdFfl/2d4b2afcc9b59a6056d1e297de599cf1/harsh.png', creator: 'Harsh Verma', linkedin: 'https://www.linkedin.com/in/harsh-verma-572167287/' ,play:'https://vermaharsh.itch.io/point-blank',description:'3D top Down shooter Game',video:''},
+        {
+          "id": 5,
+          "name": "Project ZRH",
+          "image": "https://img.itch.zone/aW1hZ2UvMjU2NTA5My8xNTI5OTM1NS5wbmc=/original/iF%2B%2Fmr.png",
+          "creator": "Raaja Das",
+          "linkedin": "https://www.linkedin.com/in/raaja-das-92797026a/",
+          "play": "https://computer-graphics-society.itch.io/project-zrh",
+          "description": "A fast-paced zombie survival game—scavenge, fight hordes, and uncover the outbreak’s secrets. Stay sharp, stay alive!",
+          "video": "https://img.itch.zone/aW1hZ2UvMjU2NTA5My8xNTI5OTM1NS5wbmc=/original/iF%2B%2Fmr.png"
+        }
       ];
     
       const puzzleGames = [
